c89a945cc841cc8f2430b59e4027026f93005cbe
shortnd <ococncol@gmail.com>
2019-01-02 11:47:58 -0500

    added seeders

--- database/factories/ChildFactory.php
+++ database/factories/ChildFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Child::class, function (Faker $faker) {
     return [
-        //
+        'first_name' => $faker->firstname(),
+        'last_name' => $faker->lastname()
     ];
 });
---
+++ database/seeds/ChildSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Child;
+use App\Checkin;
+use Illuminate\Database\Seeder;
+
+class ChildSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Child::class, 20)->create()->each(function($child) {
+            $child->addCheckin($child);
+        });
+    }
+}
