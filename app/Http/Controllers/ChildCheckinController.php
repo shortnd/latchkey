a9aae4e539c5c43e484c869f7f0d51b299996721
shortnd <ococncol@gmail.com>
2019-01-13 12:09:57 -0500

    made a really dirty way of adding totals for pm_checkout

--- app/Http/Controllers/ChildCheckinController.php
+++ app/Http/Controllers/ChildCheckinController.php
@@ -27,6 +27,7 @@ class ChildCheckinController extends Controller
             'am_checkin_time' => Carbon::now()
         ]);
         $endTime = Carbon::create(today()->format('Y'), today()->format('m'), today()->format('d'), 8, 15, 0);
+        // $endTime = Carbon::createFromTime(8, 15, 0);
         $todayAMTotalHours = $checkin->am_checkin_time->diff($endTime)->format('%H.%I');
 
         $checkinTotals->update([
@@ -40,7 +41,8 @@ class ChildCheckinController extends Controller
     public function pm_checkin(Child $child, Request $request)
     {
         $pm_checkin = $child->todaysCheckin();
-        $time = Carbon::createFromTime(15, 0, 0);
+        // $time = Carbon::createFromTime(15, 0, 0);
+        $time = Carbon::create(today()->format('Y'),today()->format('m'),today()->format('d'),15,0,0);
         $pm_checkin->update(['pm_checkin' => $request->has(['pm_checkin']), 'pm_checkin_time' => $time]);
         return back();
     }
@@ -48,9 +50,22 @@ class ChildCheckinController extends Controller
     public function pm_checkout(Child $child, Request $request)
     {
         $pm_checkout = $child->todaysCheckin();
+        $checkinTotals = $child->dailyTotal();
 
         if ($request->has('pm_checkout')) {
             $pm_checkout->update(['pm_checkout_time' => Carbon::now()]);
+
+            $pm_diff = Carbon::parse($pm_checkout->pm_checkin_time)->diff($pm_checkout->pm_checkout_time)->format('%H.%I');
+            $rollingTotalHours = $checkinTotals->total_hours + $pm_diff;
+
+            $pm_amount = $pm_diff * 4;
+            $rollingTotalAmount = $checkinTotals->total_amount + $pm_amount;
+
+            $checkinTotals->update([
+                'total_hours' => $rollingTotalHours,
+                'total_amount' => $rollingTotalAmount
+            ]);
+
         } else {
             return $errors['pm_checkout'] = 'Invalid Input';
         }
