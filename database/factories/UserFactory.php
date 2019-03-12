b49277d81f592167dea98f7e957ecf7af276c3a6
shortnd <ococncol@gmail.com>
2019-03-11 22:29:54 -0400

    updated user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,7 +16,7 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'username' => $faker->userName,
+        'username' => 'shortnd',
         'email' => 'admin@admin.com',
         'email_verified_at' => now(),
         'password' => '$2y$10$xIHdeQH4lul8fY54.nLggep31FoVoDysjO.Hzf2ACFAmzEYFCnxW.', //P@ssword!
